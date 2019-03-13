bdba356c82102fc4c88376124199536d7cb29818
regdai <1148440350@qq.com>
2019-03-13 11:36:00 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  display: grid;
+  grid-template-columns: auto 1200px auto;
+  grid-template-rows: 80px 1000px 40px;
+  grid-template-areas: "header header header"
+  ".      hero   ."
+  "footer footer footer";
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
